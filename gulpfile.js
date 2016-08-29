0173a61c8be8e932a6f6f60041bce179f3d84e0c
Jaime Gomez <gomezv9@hotmail.com>
2016-08-29 07:05:40 -0500

    install mocha, first test with mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var karma = require('karma').server;
 
 gulp.task('serve', function(){
 	browserSync.init({
@@ -8,7 +9,31 @@ gulp.task('serve', function(){
 		server:{
 			baseDir:["app"],
 			routes:{
-				'/bower_components':'bower_componenst'
+				'/bower_components':'bower_components'
+			}
+		}
+	})
+
+	gulp.watch(['app/**/*.*'])
+		.on('change',browserSync.reload);
+});
+
+gulp.task('test-browser',function(done){
+	karma.start({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	
+	})
+});
+
+gulp.task('serve-test', function(){
+	browserSync.init({
+		notify:false,
+		port:3001,
+		server:{
+			baseDir:["test","app"],
+			routes:{
+				'/bower_components':'bower_components'
 			}
 		}
 	})
